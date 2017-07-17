629537d59d9de80ea0d74750bd52d076be7e8924
Timothy Barrett <tim.barrett.jr@gmail.com>
2017-07-16 19:27:22 -0700

    Initial Commit

---
+++ AI Snap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AI Snap
+//
+//  Created by Timothy Barrett on 7/16/17.
+//  Copyright © 2017 Timothy Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
